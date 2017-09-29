4376a823046558afcb525a7b7058f7a48748b3fb
ramnrem <30977844+ramnrem@users.noreply.github.com>
2017-09-29 23:29:54 +0300

    pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp      = require('gulp'),
     sass        = require('gulp-sass'),
-    browserSync = require('browser-sync');
-    autoprefixer = require('gulp-autoprefixer');
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    pug = require('gulp-pug');
 
 gulp.task('sass', function(){
     return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('pug', function(){
+    return gulp.src('app/pug/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('app'))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -20,8 +28,8 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass', 'pug'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']); 
-    gulp.watch('app/*.html', browserSync.reload); 
-    gulp.watch('app/js/**/*.js', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/pug/**/*.pug', browserSync.reload);
 });
